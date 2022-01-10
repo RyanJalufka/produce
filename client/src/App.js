d3ac0950483e7a1e989f79b858e3e5ff58fe2d35
Kristian Saenz <ksaenz@brikit.com>
2022-01-09 23:18:02 -0800

    chore: install mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>potato turtle</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>potato turtle soup</p>
+        <Button variant="contained">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Button>
       </header>
     </div>
   );
