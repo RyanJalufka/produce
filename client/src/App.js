1e8f4508da69a770425d9a471bf59e9d72c4e215
Kristian Saenz <kristian.saenz49@gmail.com>
2022-01-26 00:39:45 -0600

    Setup private route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import {
 } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import Dashboard from  "./pages/Dashboard"
 
 function App() {
   return (
@@ -13,6 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="login" element={<Login />} />
+
+
+        <Route path="dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+
       </Routes>
     </BrowserRouter>
   );
