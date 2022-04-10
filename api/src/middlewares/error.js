6956e020db706f71d0945f8e2246b013b66b62fa
Ryan Jalufka <ryan@synthetik-technologies.com>
2022-04-09 18:43:58 -0700

    api: suppress stack trace in response

--- api/src/middlewares/error.js
+++ api/src/middlewares/error.js
@@ -28,7 +28,7 @@ const errorHandler = (err, req, res, next) => {
   const response = {
     code: statusCode,
     message,
-    ...(config.env === 'development' && { stack: err.stack }),
+    // ...(config.env === 'development' && { stack: err.stack }),
   };
 
   if (config.env === 'development') {
