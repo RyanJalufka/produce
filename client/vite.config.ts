a069a548d715b9d57aafdbfd07eeebb603436538
Ryan Jalufka <ryan@synthetik-technologies.com>
2022-04-09 21:04:54 -0700

    set title to show current MODE

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,23 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react()
-  ],
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory
+  const env = loadEnv(mode, './environments')
+  console.log("mode: ", mode)
+  return {
+    // build specific config
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: env.VITE_APP_TITLE
+          }
+        }
+      })
+    ],
+  }
 })
